34dd71babf7d33786d89c73f1679bb044cd03a57
Thomas Aldaitz <thomas.aldaitz@wildcodeschool.com>
2021-07-13 11:10:23 +0200

    Add persistance

--- admin/wild-student-management-admin.php
+++ admin/wild-student-management-admin.php
@@ -2,54 +2,84 @@
 
 function showCreateStudentForm() {
 
+    global $wpdb;
+
     if(isset($_POST['lastname']) && isset($_POST['firstname'])) {
         $lastname = htmlentities($_POST['lastname']);
         $firstname = htmlentities($_POST['firstname']);
 
-        ?>
+        $wpdb->query(
+            $wpdb->prepare(
+                "
+                    INSERT INTO {$wpdb->prefix}student (lastname, firstname)
+                    VALUES (%s, %s);
+                ",
+                $lastname,
+                $firstname
+            )
+        );
 
-        <table>
-            <thead>
-                <tr>
-                    <th>Nom</th>
-                    <th>Prénom</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        <?php echo $lastname;  ?>
-                    </td>
-                    <td>
-                        <?php echo $firstname;  ?>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        echo "<p>Nouvel étudiant enregistré !</p>";
 
-        <?php
-    } else {
+    }
+
+    $students = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}student;", OBJECT);
 
     ?>
-        <h1>Création d'étudiant</h1>
-        
-        <form method="POST">  
-            <p>
-                <label for="">Nom : </label>
-                <input type="text" name="lastname"/>
-            </p>
 
-            <p>
-                <label for="">Prénom : </label>
-                <input type="text" name="firstname"/>
-            </p>
+        <div class="container">
+            <h1>Création d'étudiant</h1>
+            
+            <form method="POST">  
+                <p>
+                    <label for="">Nom : </label>
+                    <input type="text" name="lastname"/>
+                </p>
 
-            <p>
-                <input type="submit" value="Créer">
-            </p>
-        </form>
+                <p>
+                    <label for="">Prénom : </label>
+                    <input type="text" name="firstname"/>
+                </p>
+
+                <p>
+                    <input type="submit" value="Créer">
+                </p>
+            </form>
+
+            <br>
+            <br>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nom</th>
+                        <th>Prénom</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php 
+                    
+                    foreach($students as $student) {
+
+                        echo "
+                        <tr>
+                            <td>
+                                $student->lastname
+                            </td>
+                            <td>
+                                $student->firstname
+                            </td>
+                        </tr>
+                        ";
+
+                    }
+                    
+                    ?>
+                </tbody>
+            </table>
+
+        </div>
     <?php
-    }
 }
 
 add_action( 'admin_menu', 'studentManagementInit' );
--- wild-student-management.php
+++ wild-student-management.php
@@ -4,6 +4,32 @@
  * Description:       Un plugin pour gérer des étudiants
  */
 
+function wildStuMgmtActivation() {
+    global $wpdb;
+
+    $wpdb->query("
+        CREATE TABLE {$wpdb->prefix}student(
+            id int primary key auto_increment,
+            lastname varchar(100),
+            firstname varchar(100)
+        );
+    ");
+
+}
+
+register_activation_hook( __FILE__, 'wildStuMgmtActivation' );
+
+
+function wildStuMgmtDeactivation() {
+    global $wpdb;
+
+    $wpdb->query("
+        DROP TABLE {$wpdb->prefix}student;
+    ");
+}
+
+register_deactivation_hook( __FILE__, 'wildStuMgmtDeactivation' );
+
 
  if(is_admin()) {
      require_once('admin/wild-student-management-admin.php');
